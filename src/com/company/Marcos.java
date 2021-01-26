20bc19a121ac36eab5c5baec3e5aae0be617b44b
Marcos López Santorum <mlopezsantorum@danielcastelao.org>
2021-01-26 11:05:09 +0100

    2ª cambio

--- src/com/company/Marcos.java
+++ src/com/company/Marcos.java
@@ -6,4 +6,7 @@ public class Marcos {
     public void primerCambio(){
         System.out.println(" cambio 1");
     }
+    public void segundoCambio(){
+        System.out.println(" cambio 2");
+    }
 }
